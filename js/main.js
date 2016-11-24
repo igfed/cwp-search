cfa8b880c230b9ff19393a38d247a45ad5a8cb00
Fagan <FAGAND1@ig.bz>
2016-11-24 15:17:30 -0500

    Combining sepearate projects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var modelUrl = "http://54.160.16.202:9000/api/cwpsearch?searchtype=con&lang=en&city=winnipeg&callback=?";
+tempModel = "[{\"myInfo\":{\"title\":\"Division Director\",\"State\":\"BC\",\"PostalCode\":\"V9B 2W8\",\"position\":\"Division Director\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/14479.PNG\",\"Phone\":\"2503884234\",\"LName\":\"DASHWOOD\",\"hasFrenchWebsite\":false,\"hasEnglishWebsite\":true,\"FName\":\"RANDAL\",\"Email\":\"randal.dashwood@investorsgroup.com\",\"displayOrder\":null,\"designation\":null,\"Ct_City\":\"Victoria\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/randal!dashwood\",\"Address\":\"201 - 630 Goldstream Avenue\",\"AcceptingNewClients\":true},\"myConsultantTeam\":[{\"title\":\"Senior Executive Consultant\",\"State\":\"ON\",\"PostalCode\":\"L6M 3G4\",\"position\":\"Consultant\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/14328.PNG\",\"Phone\":\"9058477776\",\"LName\":\"SPENCE\",\"hasFrenchWebsite\":false,\"hasEnglishWebsite\":true,\"FName\":\"EVERAD\",\"Email\":\"everad.spence@investorsgroup.com\",\"displayOrder\":1,\"designation\":null,\"Ct_City\":\"Oakville\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/everad!spence\",\"Address\":\"1275 North Service Road West,Unit 100\",\"AcceptingNewClients\":true},{\"title\":\"Consultant\",\"State\":\"BC\",\"PostalCode\":\"V2L 5B8\",\"position\":\"Consultant\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/15229.PNG\",\"Phone\":\"2505642310\",\"LName\":\"MOFFATT\",\"hasFrenchWebsite\":true,\"hasEnglishWebsite\":true,\"FName\":\"SCOTT\",\"Email\":\"scott.moffatt@investorsgroup.com\",\"displayOrder\":0,\"designation\":null,\"Ct_City\":\"Prince George\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/scott!moffatt\",\"Address\":\"900 - 299 Victoria Street\",\"AcceptingNewClients\":true},{\"title\":\"Consultant\",\"State\":\"ON\",\"PostalCode\":\"N7T 7T9\",\"position\":\"Consultant\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/18868.PNG\",\"Phone\":\"5193364262\",\"LName\":\"LEFEBVRE\",\"hasFrenchWebsite\":true,\"hasEnglishWebsite\":true,\"FName\":\"KATHLEEN\",\"Email\":\"kathleen.lefebvre@investorsgroup.com\",\"displayOrder\":4,\"designation\":\"MBA, CFP, GGG\",\"Ct_City\":\"Sarnia\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/kathleen!lefebvre\",\"Address\":\"1410 - 201 Front Street N.\",\"AcceptingNewClients\":true},{\"title\":\"Division Director\",\"State\":\"MB\",\"PostalCode\":\"R2C 3B3\",\"position\":\"Division Director\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/3604.PNG\",\"Phone\":\"2042221700\",\"LName\":\"SPRAGUE\",\"hasFrenchWebsite\":true,\"hasEnglishWebsite\":true,\"FName\":\"BILL\",\"Email\":\"bill.sprague@investorsgroup.com\",\"displayOrder\":3,\"designation\":null,\"Ct_City\":\"Winnipeg\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/bill!sprague\",\"Address\":\"200-1605 Regent Avenue West\",\"AcceptingNewClients\":true},{\"title\":\"Consultant\",\"State\":\"NS\",\"PostalCode\":\"B3M 0A5\",\"position\":\"Consultant\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/Silhouette_Man.png\",\"Phone\":\"9024573050\",\"LName\":\"FARELLA\",\"hasFrenchWebsite\":false,\"hasEnglishWebsite\":true,\"FName\":\"LARRY\",\"Email\":\"larry.farella@investorsgroup.com\",\"displayOrder\":2,\"designation\":null,\"Ct_City\":\"Halifax\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/larry!farella\",\"Address\":\"647 Bedford Highway,Suite 102\",\"AcceptingNewClients\":true}],\"associates\":[{\"attributes\":{\"type\":\"Associate__c\",\"url\":\"/services/data/v38.0/sobjects/Associate__c/a032C000000xPsaQAE\"},\"First_Name__c\":\"JEAN-PIERRE\",\"Last_Name__c\":\"BETIE\",\"Email__c\":\"jean-pierre.betie@groupeinvestors.com\",\"Consultant__c\":\"0032C0000045NcAQAU\",\"Display_Order__c\":0,\"Id\":\"a032C000000xPsaQAE\"}],\"assistants\":[{\"attributes\":{\"type\":\"Assistant__c\",\"url\":\"/services/data/v38.0/sobjects/Assistant__c/a012C000000WLLAQA4\"},\"First_Name__c\":\"NumberTwo\",\"Last_Name__c\":\"Testasst\",\"Email__c\":\"email@ig.com\",\"Consultant__c\":\"0032C0000045NcAQAU\",\"Display_Order__c\":0,\"Id\":\"a012C000000WLLAQA4\"},{\"attributes\":{\"type\":\"Assistant__c\",\"url\":\"/services/data/v38.0/sobjects/Assistant__c/a012C000000WLLBQA4\"},\"First_Name__c\":\"NumberOne\",\"Last_Name__c\":\"Testasst\",\"Email__c\":\"email@ig.com\",\"Consultant__c\":\"0032C0000045NcAQAU\",\"Display_Order__c\":1,\"Id\":\"a012C000000WLLBQA4\"}]},{\"myInfo\":{\"title\":\"Division Director\",\"State\":\"MB\",\"PostalCode\":\"R2C 3B3\",\"position\":\"Division Director\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/16930.PNG\",\"Phone\":\"2042221700\",\"LName\":\"DUECK\",\"hasFrenchWebsite\":false,\"hasEnglishWebsite\":true,\"FName\":\"WESLEY\",\"Email\":\"wesley.dueck@investorsgroup.com\",\"displayOrder\":null,\"designation\":null,\"Ct_City\":\"Winnipeg\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/wesley!dueck\",\"Address\":\"200-1605 Regent Avenue West\",\"AcceptingNewClients\":true},\"myConsultantTeam\":[],\"associates\":[],\"assistants\":[{\"attributes\":{\"type\":\"Assistant__c\",\"url\":\"/services/data/v38.0/sobjects/Assistant__c/a012C000000WLL5QAO\"},\"First_Name__c\":\"Welsley\",\"Last_Name__c\":\"Church\",\"Email__c\":\"wchurch@gmail.com\",\"Consultant__c\":\"0032C0000045NcEQAU\",\"Display_Order__c\":0,\"Id\":\"a012C000000WLL5QAO\"}]},{\"myInfo\":{\"title\":\"Division Director\",\"State\":\"MB\",\"PostalCode\":\"R2C 3B3\",\"position\":\"Division Director\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/3604.PNG\",\"Phone\":\"2042221700\",\"LName\":\"SPRAGUE\",\"hasFrenchWebsite\":true,\"hasEnglishWebsite\":true,\"FName\":\"BILL\",\"Email\":\"bill.sprague@investorsgroup.com\",\"displayOrder\":null,\"designation\":null,\"Ct_City\":\"Winnipeg\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/bill!sprague\",\"Address\":\"200-1605 Regent Avenue West\",\"AcceptingNewClients\":true},\"myConsultantTeam\":[{\"title\":\"Division Director\",\"State\":\"QC\",\"PostalCode\":\"H9R 0A5\",\"position\":\"Division Director\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/27657.PNG\",\"Phone\":\"5144260886\",\"LName\":\"SILVA NOGUEIRA BARROS JR\",\"hasFrenchWebsite\":false,\"hasEnglishWebsite\":true,\"FName\":\"Jose Roberto\",\"Email\":\"joseroberto.barros@inv1estorsgroup.com\",\"displayOrder\":0,\"designation\":null,\"Ct_City\":\"Pointe-Claire\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/joseroberto!barros\",\"Address\":\"6500 Trans-Canada Hwy,Suite 600\",\"AcceptingNewClients\":true}],\"associates\":[{\"attributes\":{\"type\":\"Associate__c\",\"url\":\"/services/data/v38.0/sobjects/Associate__c/a032C000000xPsdQAE\"},\"First_Name__c\":\"JEAN-PIERRE\",\"Last_Name__c\":\"BETIE\",\"Email__c\":\"jean-pierre.betie@groupeinvestors.com\",\"Consultant__c\":\"0032C0000045NcOQAU\",\"Display_Order__c\":0,\"Id\":\"a032C000000xPsdQAE\"}],\"assistants\":[{\"attributes\":{\"type\":\"Assistant__c\",\"url\":\"/services/data/v38.0/sobjects/Assistant__c/a012C000000WLL9QAO\"},\"First_Name__c\":\"Jimmy\",\"Last_Name__c\":\"Jammy\",\"Consultant__c\":\"0032C0000045NcOQAU\",\"Display_Order__c\":0,\"Id\":\"a012C000000WLL9QAO\"}]},{\"myInfo\":{\"title\":\"Executive Financial Consultant\",\"State\":\"MB\",\"PostalCode\":\"R2C 3B3\",\"position\":\"Consultant\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/18449.PNG\",\"Phone\":\"2042221700\",\"LName\":\"CLOUTIER\",\"hasFrenchWebsite\":false,\"hasEnglishWebsite\":true,\"FName\":\"MARTIN\",\"Email\":\"martin.cloutier@investors1group.com\",\"displayOrder\":null,\"designation\":null,\"Ct_City\":\"Winnipeg\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/martin!cloutier\",\"Address\":\"200-1605 Regent Avenue West\",\"AcceptingNewClients\":true},\"myConsultantTeam\":[],\"associates\":[{\"attributes\":{\"type\":\"Associate__c\",\"url\":\"/services/data/v38.0/sobjects/Associate__c/a032C000000xQYMQA2\"},\"First_Name__c\":\"JEAN-PIERRE\",\"Last_Name__c\":\"BETIE\",\"Email__c\":\"jean-pierre.betie@groupeinvestors.com\",\"Consultant__c\":\"0032C0000045WkwQAE\",\"Display_Order__c\":0,\"Id\":\"a032C000000xQYMQA2\"}],\"assistants\":[{\"attributes\":{\"type\":\"Assistant__c\",\"url\":\"/services/data/v38.0/sobjects/Assistant__c/a012C000000WLLEQA4\"},\"First_Name__c\":\"Snoopy\",\"Last_Name__c\":\"Brown\",\"Email__c\":\"snoopy.brown@noemail.com\",\"Consultant__c\":\"0032C0000045WkwQAE\",\"Display_Order__c\":0,\"Id\":\"a012C000000WLLEQA4\"}]},{\"myInfo\":{\"title\":\"Consultant\",\"State\":\"ON\",\"PostalCode\":\"P7B 3A6\",\"position\":\"Consultant\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/Silhouette_Woman.png\",\"Phone\":null,\"LName\":\"MACDONALD\",\"hasFrenchWebsite\":false,\"hasEnglishWebsite\":true,\"FName\":\"JANICE\",\"Email\":\"janice.macdonald@investorsgroup.com\",\"displayOrder\":null,\"designation\":\"CFP, MBA\",\"Ct_City\":\"Thunder Bay\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/janice!macdonald\",\"Address\":\"Unit D5, 959 Fort William Road\",\"AcceptingNewClients\":true},\"myConsultantTeam\":[{\"title\":\"Executive Financial Consultant\",\"State\":\"MB\",\"PostalCode\":\"R2C 3B3\",\"position\":\"Consultant\",\"photoUrl\":\"https://www.investorsgroup.com/en/images/ig-cwpb/profilePhotos/18449.PNG\",\"Phone\":\"2042221700\",\"LName\":\"CLOUTIER\",\"hasFrenchWebsite\":false,\"hasEnglishWebsite\":true,\"FName\":\"MARTIN\",\"Email\":\"martin.cloutier@investors1group.com\",\"displayOrder\":0,\"designation\":null,\"Ct_City\":\"Winnipeg\",\"ConsultantUrl\":\"http://cwpqa-cwp.cs59.force.com/en/martin!cloutier\",\"Address\":\"200-1605 Regent Avenue West\",\"AcceptingNewClients\":true}],\"associates\":[{\"attributes\":{\"type\":\"Associate__c\",\"url\":\"/services/data/v38.0/sobjects/Associate__c/a032C000000xQQFQA2\"},\"First_Name__c\":\"JEAN-PIERRE\",\"Last_Name__c\":\"BETIE\",\"Email__c\":\"jean-pierre.betie@groupeinvestors.com\",\"Consultant__c\":\"0032C0000045Wl3QAE\",\"Display_Order__c\":0,\"Id\":\"a032C000000xQQFQA2\"}],\"assistants\":[{\"attributes\":{\"type\":\"Assistant__c\",\"url\":\"/services/data/v38.0/sobjects/Assistant__c/a012C000000WLLDQA4\"},\"First_Name__c\":\"Kathleen\",\"Last_Name__c\":\"Testassistant\",\"Email__c\":\"kathleen.ledingham@investorsgroup.com\",\"Consultant__c\":\"0032C0000045Wl3QAE\",\"Display_Order__c\":0,\"Id\":\"a012C000000WLLDQA4\"}]}]";
+// Process the local prefetched data
+var suggestions = {};
+	suggestions.locations = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.whitespace,
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		prefetch: '../external/app/tribal/data/cities.json'
+	});
+	suggestions.consultants = new Bloodhound({
+		// datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
+		datumTokenizer: Bloodhound.tokenizers.whitespace,
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		prefetch: '../external/app/tribal/data/names.json'
+	});
+	suggestions.postalCode = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.whitespace,
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		prefetch: '../external/app/tribal/data/postal-code.json'
+	});
+
 // Get current location
 function getCoordinates() {
 	if (!navigator.geolocation){
@@ -23,29 +44,52 @@ function getCoordinates() {
 	return result;
 }
 
+// Get the results
+function getSearchResults() {
+	var request = $.ajax({
+		url: modelUrl,
+		// data: ,
+		dataType: "string"
+	});
 
-// Process the data
-var locations = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: '../external/app/tribal/data/cities.json'
-});
-var consultants = new Bloodhound({
-  // datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: '../external/app/tribal/data/names.json'
-});
-var postalCode = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: '../external/app/tribal/data/postal-code.json'
-});
+	request.done(function( msg ) {
+		var result = JSON.parse(tempModel)
+		displaySearchResults(result);
+	});
 
+	request.fail(function( result ) {
+		console.log('Data could not be retrieved, please try again', result.status + ' ' + result.statusText);
+		var result = JSON.parse(tempModel)
+		displaySearchResults(result);
+	});
+}
+
+function displaySearchResults( json ) {
+	//Grab the inline template
+	var template = document.getElementById('template').innerHTML;
+	//Parse it (optional, only necessary if template is to be used again)
+	Mustache.parse(template);
+
+	//Render the data into the template
+	console.log(json);
+	var rendered;
+	for (i=0; i< json.length-1; i++) {
+		rendered += Mustache.render(template, json[i]);
+	}
+
+	//Overwrite the contents of #target with the rendered HTML
+	$('#results-container').html(rendered);
+}
+
+
+
+
+//Init everything
 $(function() {
 	$('.typeahead').typeahead({ highlight: true },
-		{ name: 'consultants', source: consultants },
-		{ name: 'locations', source: locations },
-		{ name: 'postalCode', source: postalCode}
+		{ name: 'consultants', source: suggestions.consultants },
+		{ name: 'locations', source: suggestions.locations },
+		{ name: 'postalCode', source: suggestions.postalCode }
 	);
+	getSearchResults();
 });
